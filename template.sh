e31b0d237c4c7fd11fcc193a7d910c02e5f441ce
Subrata <subratasamrat2@gmail.com>
2026-09-12 11:38:49 +0530

    folder structure

---
+++ template.sh
@@ -0,0 +1,14 @@
+mkdir -p src
+mkdir -p reserach
+
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+
+touch setup.py
+touch app.py
+touch research/trials.ipynb
+touch requirments.txt
+
+echo "directory and file created successfully"
